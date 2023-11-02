31c9c8915e0c3773fef938844b042c13972384e9
Maxim Kurepov <californium251@gmail.com>
2023-11-02 16:23:00 +0400

    another try

--- src/slices/gameSlice.tsx
+++ src/slices/gameSlice.tsx
@@ -12,7 +12,7 @@ import {
     allPlayersPassed,
     canPlayerAdd,
     onlyOnePlayerHasCards
-} from "@/utils/utils";
+} from "../utils/utils";
 
 const initialState: {
     cards: Array<CardType>,
