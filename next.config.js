5b87f3023234a142e915b5c747f9a6015fb0af4a
Maxim Kurepov <californium251@gmail.com>
2023-11-02 23:33:42 +0400

    nex config changed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+
 const nextConfig = {
-    output: 'standalone',
-    webpack: (config, { isServer }) => {
-        config.resolve.alias['~'] = path.join(__dirname, 'src')
-        return config
-    },
     i18n: {
         locales: ['en', 'es'],
         defaultLocale: 'en',
