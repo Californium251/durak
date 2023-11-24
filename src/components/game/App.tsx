35c1dd835658a6f1731fcdf7c29bd84b7fdd1ba0
Maxim Kurepov <californium251@gmail.com>
2023-11-24 18:44:32 +0400

    try to deploy

--- src/components/game/App.tsx
+++ src/components/game/App.tsx
@@ -13,6 +13,7 @@ import Transport from "@/utils/Transport"
 import { socket } from "@/utils/socket"
 
 const App: FC = () => {
+    console.log(process.env.FRONTEND_URL);
     const transport = new Transport();
     const { token } = useAuth().auth;
     const path = usePathname();
