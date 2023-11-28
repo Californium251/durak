ee1d763a51b12238800a066f5b9d1c0b078b73f0
Maxim Kurepov <californium251@gmail.com>
2023-11-28 11:11:20 +0400

    another try to deploy

--- src/components/signup/SignUpForm.tsx
+++ src/components/signup/SignUpForm.tsx
@@ -20,6 +20,7 @@ const SignUpForm = () => {
         onSubmit: async (values) => {
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(values.password, salt);
+            console.log(process.env.SOCKET_IO_URL);
             try {
                 const res = await axios.post(`${serverAddress}/signup`, {
                     method: 'POST',
