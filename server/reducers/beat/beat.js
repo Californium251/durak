eda2a9a41a5d5de01da2635bfa613b7e66049e18
Maxim Kurepov <californium251@gmail.com>
2023-12-04 20:42:14 +0400

    bug when not appropriate cards were droped now fixed

--- server/reducers/beat/beat.js
+++ server/reducers/beat/beat.js
@@ -1,11 +1,15 @@
 const beatCard = require('./utils/beatCard');
 const removeCard = require('../addCard/utils/player/removeCard');
 const onlyOnePlayerHasCards = require('./utils/onlyOnePlayerHasCards');
+const doesCardBeat = require('./utils/doesCardBeat');
 
 const beat = (game, data) => {
     const { players, table, cards, trumpDrawn } = game.data;
     const { playerId, card1, card2, trump } = data;
     const player = players.find((p) => p.user._id.toString() === playerId);
+    if (!doesCardBeat(card1, card2, trump)) {
+        return game;
+    }
     const updatedTable = beatCard(table, card1, card2, trump);
     const updatedPlayer = removeCard(player, card2);
     const updatedPlayers = players.map((p) => {
--- server/reducers/beat/utils/beatCard.js
+++ server/reducers/beat/utils/beatCard.js
@@ -2,9 +2,6 @@ const doesCardBeat = require('./doesCardBeat');
 const areCardsEqual = require('./areCardsEqual');
 
 const beatCard = (table, card1, card2, trump) => {
-    if (!doesCardBeat(card1, card2, trump)) {
-        return table;
-    }
     const newTable = table.map((cardPair) => {
         if (areCardsEqual(cardPair[0], card1)) {
             return [card1, card2];
